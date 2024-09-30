af39eb5d65ea148d2f225b27f1d020f9983acbdb
Revan Kurniawan <rvnkrwn@hotmail.com>
2024-09-30 12:17:33 +0700

    setup: layout style with page

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <AppHeader />
+  <AppSidebar />
+  <slot />
+</template>
+
+<script setup lang="ts">
+
+import AppHeader from "~/components/AppHeader.vue";
+import AppSidebar from "~/components/AppSidebar.vue";
+</script>
+
+<style scoped>
+
+</style>
