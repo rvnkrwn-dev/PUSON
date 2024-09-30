8ef16ced9dc0d011ee0ae4c3cf204774e44ea330
Revan Kurniawan <rvnkrwn@hotmail.com>
2024-09-30 12:17:51 +0700

    setup: tailwindcss config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
     content: [
         './node_modules/preline/preline.js',
+        './layouts/*.{vue,js,ts, html}',
+        './pages/*.{vue,js,ts, html}',
+        './pages/**/*.{vue,js,ts, html}',
+        './pages/**/**/*.{vue,js,ts, html}',
+        './components/*.{vue,js,ts, html}',
+        './components/**/*.{vue,js,ts, html}',
+        './components/**/**/*.{vue,js,ts, html}',
     ],
     theme: {
         extend: {},
