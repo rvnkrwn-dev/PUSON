3169553228e565d211c103cc573acedbd3f9e14b
fazza_abiyyu <67826596+chi-abiyyu@users.noreply.github.com>
2024-10-10 18:14:11 +0700

    email_helper.py

--- backend/app/email_helper.py
+++ backend/app/email_helper.py
@@ -7,14 +7,8 @@ def send_email(subject, body, recipient_email):
     smtp_host = "smtp.gmail.com"
     smtp_port = 587
     smtp_user = "2211102010@ittelkom-pwt.ac.id"
-    smtp_password = "??????????"
+    smtp_password = "?????"
     from_addr = "2211102010@ittelkom-pwt.ac.id"
-
-    # Membuat objek MIMEMultipart
-    msg = MIMEMultipart()
-    msg["From"] = "puson.support"
-    msg["To"] = recipient_email
-    msg["Subject"] = subject
 
     # Menambahkan body ke email
     msg.attach(MIMEText(body, "plain"))
