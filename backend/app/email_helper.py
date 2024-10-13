7a8b42f7d6feeabaac34ed96040e57348f91f92c
fazza_abiyyu <67826596+chi-abiyyu@users.noreply.github.com>
2024-10-13 10:49:08 +0700

    email_helper.py

--- backend/app/email_helper.py
+++ backend/app/email_helper.py
@@ -7,17 +7,19 @@ def send_email(subject, body, recipient_email):
     smtp_host = "smtp.gmail.com"
     smtp_port = 587
     smtp_user = "2211102010@ittelkom-pwt.ac.id"
-    smtp_password = "?????"
+    smtp_password = "1700428480@Chiel.yu"
     from_addr = "2211102010@ittelkom-pwt.ac.id"
 
-    # Menambahkan body ke email
+    msg = MIMEMultipart()
+    msg['From'] = from_addr
+    msg['To'] = recipient_email
+    msg['Subject'] = subject
+
     msg.attach(MIMEText(body, "plain"))
 
-    # Membuat koneksi ke server SMTP
     server = smtplib.SMTP(smtp_host, smtp_port)
     server.starttls()
     server.login(smtp_user, smtp_password)
 
-    # Mengirim email
     server.sendmail(from_addr, recipient_email, msg.as_string())
     server.quit()
