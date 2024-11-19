23b65985177813b49861d7c00566c1b646b7cf9b
fazza_abiyyu <1700428480@qq.com>
2024-11-19 18:15:48 +0700

    Update __init__.py

--- backend/app/controllers/__init__.py
+++ backend/app/controllers/__init__.py
@@ -3,9 +3,14 @@ from .login import login_bp
 from .refresh import refresh_bp
 from .logout import logout_bp
 from .user import user_bp
+from .user import users_bp
 from .forget_password import forget_password_bp
 from .reset_password import reset_password_bp
-
+from .puskesmas import puskesmas_bp
+from .posyandu import posyandu_bp
+from .anak import anak_bp
+from .pemeriksaan import pemeriksaan_bp
+from .stunting import stunting_bp
 
 
 def init_auth_blueprints(app):
@@ -14,5 +19,11 @@ def init_auth_blueprints(app):
     app.register_blueprint(refresh_bp, url_prefix="/auth")
     app.register_blueprint(logout_bp, url_prefix="/auth")
     app.register_blueprint(user_bp, url_prefix="/auth")
+    app.register_blueprint(users_bp, url_prefix="/auth")
     app.register_blueprint(forget_password_bp, url_prefix='/auth')
     app.register_blueprint(reset_password_bp, url_prefix='/auth')
+    app.register_blueprint(puskesmas_bp, url_prefix="/auth")
+    app.register_blueprint(posyandu_bp, url_prefix="/auth")
+    app.register_blueprint(anak_bp, url_prefix="/auth")
+    app.register_blueprint(pemeriksaan_bp, url_prefix="/auth")
+    app.register_blueprint(stunting_bp, url_prefix="/auth")
