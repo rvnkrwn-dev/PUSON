cf00c2eef4936fb7b7e009b494459a6df2eaa9d5
Revan Kurniawan <rvnkrwn@hotmail.com>
2024-11-26 13:43:05 +0700

    chore and update

--- frontend/components/AppSidebar.vue
+++ frontend/components/AppSidebar.vue
@@ -17,8 +17,8 @@
       <!-- Content -->
       <div
           class="h-full overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300">
-        <nav class="hs-accordion-group p-3 w-full flex flex-col flex-wrap" data-hs-accordion-always-open>
-          <ul class="flex flex-col space-y-1">
+        <nav class="hs-accordion-group p-3 h-full w-full flex flex-col flex-wrap" data-hs-accordion-always-open>
+          <ul class="h-full flex flex-col space-y-1 relative">
             <li>
               <NuxtLink active-class="bg-gray-100" class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
                  to="/">
@@ -62,9 +62,9 @@
               <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 .5a1.746 1.746 0 0 0-1 3.18v1.4c-.9.13-1.74.42-2.5.86L7.39 4.35c.19-.52.14-1.12-.2-1.6C6.84 2.26 6.3 2 5.75 2c-.35 0-.7.1-1 .32c-.79.55-.99 1.64-.43 2.43c.34.49.88.75 1.43.75l1.18 1.68c-.43.43-.77.91-1.06 1.44c-.2-.08-.41-.12-.62-.12c-.45 0-.9.17-1.25.5c-.67.7-.67 1.8 0 2.5c.29.27.64.42 1 .5c0 .54.07 1.06.18 1.56l-1.31.35c-.31-.26-.71-.41-1.12-.41c-.15 0-.31 0-.46.06a1.75 1.75 0 0 0-1.23 2.15C1.27 16.5 2 17 2.75 17c.15 0 .3 0 .46-.06c.57-.16 1-.58 1.18-1.1l1.51-.41c.45.79 1.05 1.49 1.75 2.07l-1.1 2c-.55.08-1.05.39-1.34.92a1.749 1.749 0 1 0 3.08 1.66c.28-.52.27-1.12.02-1.61l1.07-1.97c.81.32 1.69.5 2.62.5h.18c-.13.26-.18.56-.18.88c.08.92.84 1.62 1.75 1.62h.13c.97-.08 1.69-.92 1.62-1.88c-.04-.5-.29-.94-.65-1.23c.47-.21.92-.48 1.34-.79l2.34 2.34c-.1.56.06 1.13.47 1.56c.35.33.8.5 1.25.5s.9-.17 1.25-.5c.67-.7.67-1.8 0-2.5c-.35-.33-.8-.5-1.25-.5c-.1 0-.2 0-.31.03l-2.34-2.34c.49-.65.87-1.39 1.11-2.19h1.11A1.746 1.746 0 0 0 23 13a1.746 1.746 0 0 0-3.18-1H19c0-1.57-.5-3-1.4-4.19l1.34-1.34c.11.03.21.03.31.03c.45 0 .9-.17 1.25-.5c.67-.69.67-1.8 0-2.5c-.35-.33-.8-.5-1.25-.5s-.9.17-1.25.5c-.41.43-.57 1-.47 1.56L16.19 6.4c-.92-.69-2-1.15-3.19-1.32v-1.4A1.746 1.746 0 0 0 12 .5M12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5s5 2.24 5 5s-2.24 5-5 5m-1.5-8C9.67 9 9 9.67 9 10.5s.67 1.5 1.5 1.5s1.5-.67 1.5-1.5S11.33 9 10.5 9m3.5 4c-.55 0-1 .45-1 1s.45 1 1 1s1-.45 1-1s-.45-1-1-1"/></svg>
               Stunting
             </NuxtLink></li>
-            <li><NuxtLink
+            <li class="absolute bottom-6 w-full"><NuxtLink
                 active-class="bg-gray-100"
-                class="cursor-pointer w-full flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 hidden"
+                class="cursor-pointer w-full flex items-center justify-center bg-red-500 text-white gap-x-2 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-red-700"
                 @click="handleLogout">
               <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24"><path fill="currentColor" d="M4 12a1 1 0 0 0 1 1h7.59l-2.3 2.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0l4-4a1 1 0 0 0 .21-.33a1 1 0 0 0 0-.76a1 1 0 0 0-.21-.33l-4-4a1 1 0 1 0-1.42 1.42l2.3 2.29H5a1 1 0 0 0-1 1M17 2H7a3 3 0 0 0-3 3v3a1 1 0 0 0 2 0V5a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-3a1 1 0 0 0-2 0v3a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V5a3 3 0 0 0-3-3"/></svg>
               Sign Out
@@ -78,23 +78,42 @@
 </template>
 
 <script setup lang="ts">
+import Swal from "sweetalert2";
+
 const {logout} = useAuth()
 
 const handleLogout = async () => {
-  const confirmLogout = confirm("Apakah Anda yakin ingin logout?");
-  if (!confirmLogout) return; // Jika pengguna membatalkan, keluar dari fungsi
+  Swal.fire({
+    title: "Anda yakin?",
+    text: "Anda ingin keluar dari sini!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    cancelButtonText: "Batal",
+    confirmButtonText: "Ya, Keluar!"
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+      try {
+        // Memanggil fungsi logout dari useAuth
+        await logout();
 
-  try {
-    // Memanggil fungsi logout dari useAuth
-    await logout();
+        // Redirect atau tindakan lain setelah logout berhasil
+        return await navigateTo('/login')
+      } catch (error) {
+        await Swal.fire({
+          position: "bottom-end",
+          icon: "error",
+          title: "Terjadi kesalahan saat logout.",
+          showConfirmButton: false,
+          timer: 1500,
+          toast: true
+        });
+      } finally {
 
-    // Redirect atau tindakan lain setelah logout berhasil
-    alert('Anda telah berhasil logout.');
-    window.location.href = '/login'; // Redirect ke halaman login atau halaman lain
-  } catch (error) {
-    console.error('Gagal logout:', error);
-    alert('Terjadi kesalahan saat logout.');
-  }
+      }
+    }
+  });
 };
 </script>
 
