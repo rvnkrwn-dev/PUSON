1766c69d7978aec1d48396415b77aea673897be2
Revan Kurniawan <rvnkrwn@hotmail.com>
2024-11-26 13:18:09 +0700

    chore and update

--- frontend/pages/anak/index.vue
+++ frontend/pages/anak/index.vue
@@ -59,7 +59,7 @@
                       id="hs-table-search"
                       v-model="searchQuery"
                       class="py-2 px-3 ps-9 block w-full border border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-                      placeholder="Search for Anak"
+                      placeholder="Muhammad Rizqi"
                   />
                   <div class="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-3">
                     <svg class="size-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
--- frontend/server/api/chat-bot.get.ts
+++ frontend/server/api/chat-bot.get.ts
@@ -4,7 +4,7 @@ export default defineEventHandler(async (event) => {
     try {
         const genAI = new GoogleGenerativeAI("AIzaSyAfEl7SIVhQ0x03NupoPAbw03jmzr60Bq4");
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-        const prompt = "Write a story about a magic backpack."
+        const prompt = "Tuliskan lagu indonesia raya."
 
         const result = await model.generateContent(prompt);
         const response = await result.response;
